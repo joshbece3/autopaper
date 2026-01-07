38b5060a2c8320c09b5b565dc075204815d9124f
joshbece3 <joshhoom2009@gmail.com>
2026-01-07 22:34:29 +0800

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-
-# Ask the user for the agent key
-read -p "Enter the agent key: " AGENTKEY
-
-# Define the target directory and file
-CONFIG_DIR="fabric_server/config"
-TARGET_FILE="$CONFIG_DIR/playit-fabric-config.cfg"
-
-# Check if the directory exists, if not, create it
-if [[ ! -d "$CONFIG_DIR" ]]; then
-    echo "Directory $CONFIG_DIR does not exist. Creating it now..."
-    mkdir -p "$CONFIG_DIR"
-    echo "Directory $CONFIG_DIR created."
+#input
+read -p "key:" key
+#setup
+dir="fabric_server/config"
+file="$dir/playit-fabric-config.cfg"
+#check
+if [[ ! -d "$dir" ]];then
+mkdir -p "$dir"
 fi
-
-# Create the configuration file with the provided agent key and other settings
-cat <<EOL > "$TARGET_FILE"
-agent-secret=$AGENTKEY
+#config
+cat <<EOL > "$file"
+agent-secret=$key
 autostart=true
 mc-timeout-seconds=90000
 EOL
-
-echo "Configuration file created at $TARGET_FILE with the provided agent key."
+echo "done:$file"
